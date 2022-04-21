43de3d81862c533a73bc5920df326bdb5bc75dea
NachoBerardo <101335543+NachoBerardo@users.noreply.github.com>
2022-04-21 14:30:03 -0300

    Ej Clase

---
+++ Assets/Scripts/Ejercicio_Clase.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejercicio_Clase : MonoBehaviour
+{
+
+    public int num1, num2;
+
+    void Start()
+    {
+        if (num1 > num2)
+        {
+            while (num1 >= num2)
+            {
+                Debug.Log(num2);
+                num2++;
+            }
+        }
+
+        else if (num1 < num2)
+        {
+            while (num1 <= num2)
+            {
+                Debug.Log(num1);
+                num1++;
+            }
+        }
+
+        else
+        {
+            Debug.Log("No pueden ser iguales");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
